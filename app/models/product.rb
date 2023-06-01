687201beff6f1cf68f164f2efbebe34caafbfff3
Maria Efimova <marivaefi@gmail.com>
2023-06-01 13:36:18 +0300

    Added hook for deleting cart with items inside

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,13 +13,14 @@ class Product < ApplicationRecord
   validates :title, length: {minimum: 10}
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
+
   private
 
-  # ensure that there are no line items referencing this product
-  def ensure_not_referenced_by_any_line_item
-    unless line_items.empty?
-      errors.add(:base, 'Line Items present')
-      throw :abort
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
     end
-  end
 end
